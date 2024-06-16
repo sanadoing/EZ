d154bbdcff74d0832f11d4d6c1badb639896b90e
mountrive33 <mountrive33@gmail.com>
2024-06-16 18:26:00 +0900

    [sana] test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import TheWelcome from "../components/TheWelcome.vue";
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <div>sana test</div>
   </main>
 </template>
